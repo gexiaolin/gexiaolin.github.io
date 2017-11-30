88ccdf0668c0e77020cfd9e970c6661ae0c0e26d
gexiaolin <gexiaolin@smzdm.com>
2017-11-30 17:34:43 +0800

    创建vue项目，完善基本结构

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+    <div id="app">
+        <editorStyle></editorStyle>
+        <editorMarkdown></editorMarkdown>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue';
+    import Promise from 'promise-polyfill';
+    import jsPolyfill from './commons/js/jsPolyfill';
+    import editorStyle from './components/editorStyle';
+    import editorMarkdown from './components/editorMarkdown';
+
+    Vue.use(jsPolyfill);
+
+    export default {
+        name: 'root',
+        data () {
+            return {
+                styleNumber: 0,
+                styleString: [
+                `body {
+                    background: #ddd;
+                }`]
+            };
+        },
+        components: {
+            editorStyle,
+            editorMarkdown
+        },
+        mounted () {
+            !window.Promise ? window.Promise = Promise : null;
+            this.initResume();
+        },
+        methods: {
+            async initResume () {
+                await this.showStyle();
+            },
+            showStyle () {
+                return new Promise((res, rej) => {
+
+                });
+            }
+        }
+    };
+</script>
+
+<style lang="scss">
+    @import 'src/commons/styles/base.scss';
+
+    html, body {
+        height: 100%;
+    }
+</style>
---
+++ src/components/editorStyle.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="style-wrap">
+        <pre v-html="highlightedCode"></pre>
+    </div>
+</template>
+
+<script>
+    import Prism from 'prismjs'
+
+    export default {
+        data () {
+            return {
+                code: `* {
+    transition: all ease .2s;
+}
+
+body {
+    background: #f8f8f8;
+}
+
+.style-wrap {
+    width: 44%;
+    border: 1px solid #ccc;
+    overflow: auto;
+    -webkit-box-sizing: border-box;
+            box-sizing: border-box;
+    padding: 10px 30px;
+
+    position: absolute;
+    left: 5%;
+    top: 30px;
+    bottom: 30px;
+}
+
+pre {
+    color: #fff;
+}
+.token.selector {
+    color: #b3da4b;
+}
+.token.punctuation {
+    color: #fff;
+}
+.token.property {
+    color: #7fd4d4;
+}`
+            };
+        },
+        computed: {
+            highlightedCode: function () {
+              return Prism.highlight(this.code, Prism.languages.css)
+            }
+        }
+    };
+</script>
+
+<style>
+    * {
+        transition: all ease .2s;
+    }
+
+    body {
+        background: #f8f8f8;
+    }
+
+    .style-wrap {
+        width: 44%;
+        border: 1px solid #ccc;
+        overflow: auto;
+        -webkit-box-sizing: border-box;
+                box-sizing: border-box;
+        padding: 10px 30px;
+        background-color: #272823;
+
+        position: absolute;
+        left: 4%;
+        top: 30px;
+        bottom: 30px;
+    }
+
+    .style-wrap pre {
+        color: #fff;
+    }
+    .token.selector {
+        color: #b3da4b;
+    }
+    .token.punctuation {
+        color: #fff;
+    }
+    .token.property {
+        color: #7fd4d4;
+    }
+
+    .markdown-wrap {
+        width: 44%;
+        border: 1px solid #ccc;
+        overflow: auto;
+        -webkit-box-sizing: border-box;
+                box-sizing: border-box;
+        padding: 10px 30px;
+        background-color: #fff;
+
+        position: absolute;
+        right: 4%;
+        top: 30px;
+        bottom: 30px;
+    }
+</style>
