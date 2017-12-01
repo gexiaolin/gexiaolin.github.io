7bf6677a756153653b53f9e0b313ee32f12bbf84
gexiaolin <gexiaolin@smzdm.com>
2017-12-01 11:36:22 +0800

    完成css代码插入后与样式同步的功能

--- src/components/editorStyle.vue
+++ src/components/editorStyle.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="style-wrap">
+    <div class="style-wrap" ref="styleWrap">
         <pre v-html="highlightedCode"></pre>
+        <div class="work-style" v-html="workCode"></div>
     </div>
 </template>
 
@@ -8,101 +9,27 @@
     import Prism from 'prismjs'
 
     export default {
-        data () {
-            return {
-                code: `* {
-    transition: all ease .2s;
-}
-
-body {
-    background: #f8f8f8;
-}
-
-.style-wrap {
-    width: 44%;
-    border: 1px solid #ccc;
-    overflow: auto;
-    -webkit-box-sizing: border-box;
-            box-sizing: border-box;
-    padding: 10px 30px;
-
-    position: absolute;
-    left: 5%;
-    top: 30px;
-    bottom: 30px;
-}
-
-pre {
-    color: #fff;
-}
-.token.selector {
-    color: #b3da4b;
-}
-.token.punctuation {
-    color: #fff;
-}
-.token.property {
-    color: #7fd4d4;
-}`
-            };
+        props: {
+            code: String
         },
         computed: {
-            highlightedCode: function () {
-              return Prism.highlight(this.code, Prism.languages.css)
+            highlightedCode () {
+                return Prism.highlight(this.code, Prism.languages.css);
+            },
+            workCode () {
+                return `<style>${this.code}</style>`;
+            }
+        },
+        methods: {
+            goBottom () {
+                this.$refs.styleWrap.scrollTop = 999999;
             }
         }
     };
 </script>
 
 <style>
-    * {
-        transition: all ease .2s;
-    }
-
-    body {
-        background: #f8f8f8;
-    }
-
-    .style-wrap {
-        width: 44%;
-        border: 1px solid #ccc;
-        overflow: auto;
-        -webkit-box-sizing: border-box;
-                box-sizing: border-box;
-        padding: 10px 30px;
-        background-color: #272823;
-
-        position: absolute;
-        left: 4%;
-        top: 30px;
-        bottom: 30px;
-    }
-
-    .style-wrap pre {
-        color: #fff;
-    }
-    .token.selector {
-        color: #b3da4b;
-    }
-    .token.punctuation {
-        color: #fff;
-    }
-    .token.property {
-        color: #7fd4d4;
-    }
-
-    .markdown-wrap {
-        width: 44%;
-        border: 1px solid #ccc;
-        overflow: auto;
-        -webkit-box-sizing: border-box;
-                box-sizing: border-box;
-        padding: 10px 30px;
-        background-color: #fff;
-
-        position: absolute;
-        right: 4%;
-        top: 30px;
-        bottom: 30px;
+    .work-style {
+        display: none;
     }
 </style>
